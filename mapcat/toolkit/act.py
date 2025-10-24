4084a0b0a917ab74693e3428f1009957d5a45248
Josh Borrow <josh@joshborrow.com>
2025-10-24 09:31:48 -0400

    Added new parser with telescope option

--- mapcat/toolkit/act.py
+++ mapcat/toolkit/act.py
@@ -12,7 +12,7 @@ from mapcat.database import DepthOneMapTable, TODDepthOneTable
 # from astropy.coordinates import SkyCoord
 
 
-def extract_string(input) -> str:
+def extract_string(input: bytes) -> str:
     return str(input).replace("b'", "").replace("'", "")
 
 
@@ -42,10 +42,11 @@ def parse_filenames(base: str, relative_to: Path) -> dict[str, str]:
         "time": Path(base + "_time.fits"),
     }
 
+    # In case e.g. there is no inverse-variance map.
     return {x: str(y.relative_to(relative_to)) for x, y in paths.items() if y.exists()}
 
 
-def create_objects(base: str, relative_to: Path) -> DepthOneMapTable:
+def create_objects(base: str, relative_to: Path, telescope: str) -> DepthOneMapTable:
     filenames = parse_filenames(base=base, relative_to=relative_to)
     file_info = parse_info_file(path=relative_to / filenames["info"])
 
@@ -54,7 +55,7 @@ def create_objects(base: str, relative_to: Path) -> DepthOneMapTable:
             obs_id=obs_id,
             pwv=None,
             ctime=float(obs_id[4:14]),
-            telescope="lat",
+            telescope=telescope,
             tube_slot=file_info["tube_slot"],
             frequency=file_info["frequency"],
         )
@@ -77,23 +78,61 @@ def create_objects(base: str, relative_to: Path) -> DepthOneMapTable:
     return depth_one_map
 
 
-def glob(input_glob: str, relative_to: Path) -> list[DepthOneMapTable]:
+def glob(input_glob: str, relative_to: Path, telescope: str) -> list[DepthOneMapTable]:
     maps = []
 
     for map_file in relative_to.glob(input_glob):
         base = str(map_file).replace("_map.fits", "")
-        depth_one_map = create_objects(base=base, relative_to=relative_to)
+        depth_one_map = create_objects(
+            base=base, relative_to=relative_to, telescope=telescope
+        )
         maps.append(depth_one_map)
 
     return maps
 
 
+HELP_TEXT = """Use this utility to ingest depth-1 maps created by the ACT mapmaker.
+This reads the _info.hdf files to extract relevant metadata, such as the TODs
+that were used in the file. Uses a glob pattern to ingest items. The glob
+should list the '*_map.fits' files, and is relative to the top level directory.
+"""
+
+USAGE = """Imagine you have a directory /my/path/to/maps, containing:
+
+17577    17578     17579
+
+Each directory contains multiple maps, e.g.
+
+17577/depth1_1757723727_map.fits
+17577/depth1_1757723727_ivar.fits
+17577/depth1_1757723727_time.fits
+17577/depth1_1757723727_info.hdf
+17577/depth1_1757748298_map.fits
+17577/depth1_1757748298_ivar.fits
+17577/depth1_1757748298_time.fits
+17577/depth1_1757748298_info.hdf
+
+You should pass '/my/path/to/maps' as 'relative-to', and
+'*/*_map.fits' as the glob pattern.
+"""
+
+
 def main():
-    import sys
+    import argparse as ap
 
     from mapcat.helper import settings
 
+    parser = ap.ArgumentParser(prog="actingest", usage=USAGE, description=HELP_TEXT)
+
+    parser.add_argument("-r", "--relative-to", type=Path, required=True)
+
+    parser.add_argument("-g", "--glob", type=str, required=True)
+
+    parser.add_argument("-t", "--telescope", type=str, default="act")
+
+    args = parser.parse_args()
+
     with settings.session() as session:
-        maps = glob(sys.argv[1], Path(sys.argv[2]))
+        maps = glob(args.glob, args.relative_to, args.telescope)
         session.add_all(maps)
         session.commit()

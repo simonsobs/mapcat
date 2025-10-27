41cf3da12da7e2bdc770b51cd990b97d878cb9e1
Josh Borrow <josh@joshborrow.com>
2025-10-27 14:56:53 -0400

    Formatting

--- mapcat/database/__init__.py
+++ mapcat/database/__init__.py
@@ -4,8 +4,8 @@ Table definitions
 
 from .atomic_coadd import AtomicMapCoaddTable
 from .atomic_map import AtomicMapTable
-from .depth_one_map import DepthOneMapTable
 from .depth_one_coadd import DepthOneCoaddTable
+from .depth_one_map import DepthOneMapTable
 from .pipeline_information import PipelineInformationTable
 from .pointing_residual import PointingResidualTable
 from .processing_status import ProcessingStatusTable

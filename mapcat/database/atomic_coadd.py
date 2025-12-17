628f5bab014307a22802c53b706e8780ad0c6348
David Nguyen <david.nguyen@yale.edu>
2025-12-17 22:21:29 +0000

    add coadd self-referential relationship

--- mapcat/database/atomic_coadd.py
+++ mapcat/database/atomic_coadd.py
@@ -9,7 +9,7 @@ from sqlmodel import Field, Relationship, SQLModel
 if TYPE_CHECKING:
     from .atomic_map import AtomicMapTable
 
-from .links import AtomicMapToCoaddTable
+from .links import AtomicMapToCoaddTable, CoaddMapToCoaddTable
 
 
 class AtomicMapCoaddTable(SQLModel, table=True):
@@ -29,4 +29,22 @@ class AtomicMapCoaddTable(SQLModel, table=True):
     atomic_maps: list["AtomicMapTable"] = Relationship(
         back_populates="coadds",
         link_model=AtomicMapToCoaddTable,
+    )
+    
+    child_coadds: list["AtomicMapCoaddTable"] = Relationship(
+        back_populates="parent_coadds",
+        link_model=CoaddMapToCoaddTable,
+        sa_relationship_kwargs={
+            "primaryjoin": "AtomicMapCoaddTable.coadd_id == CoaddMapToCoaddTable.parent_coadd_id",
+            "secondaryjoin": "AtomicMapCoaddTable.coadd_id == CoaddMapToCoaddTable.child_coadd_id",
+        },
+    )
+    
+    parent_coadds: list["AtomicMapCoaddTable"] = Relationship(
+        back_populates="child_coadds",
+        link_model=CoaddMapToCoaddTable,
+        sa_relationship_kwargs={
+            "primaryjoin": "AtomicMapCoaddTable.coadd_id == CoaddMapToCoaddTable.child_coadd_id",
+            "secondaryjoin": "AtomicMapCoaddTable.coadd_id == CoaddMapToCoaddTable.parent_coadd_id",
+        },
     )

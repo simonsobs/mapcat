c141f119741cedd578b8c9208447ad72512ce132
John Orlowski-Scherer <jackorlowskischerer@gmail.com>
2026-02-12 13:26:16 -0500

    feat: update docs and make composite ID

--- mapcat/database/sky_coverage.py
+++ mapcat/database/sky_coverage.py
@@ -2,34 +2,47 @@
 Sky coverage table.
 """
 
-from sqlmodel import Field, Relationship, SQLModel
+from sqlmodel import Field, Relationship, Index, SQLModel
 
 from .depth_one_map import DepthOneMapTable
 
 
 class SkyCoverageTable(SQLModel, table=True):
     """
-    Table for tracking sky coverage for a depth one map. x and y are 0->36 and
-    0-18 respectively for CAR patches with 10x10 degrees each.
+    Table for tracking sky coverage patches with non-zero overlap with a given depth one map. 
+    x and y are 0->36 and 0-18 respectively for CAR patches with 10x10 degrees each.
 
     Attributes
     ----------
-    id : str
-        Internal ID of the sky coverage
-    map_name : str
-        Name of depth 1 map being tracked. Foreign into DepthOneMap
-    patch_coverage : str
-        String which represents the sky coverage of the d1map
+    id : uuid.UUID
+        Unique ID of this coverage instance
+    map : DepthOneMapTable 
+       Depth 1 map being tracked. Foreign into DepthOneMap
+    map_id : int 
+       ID of depth 1 map being tracked
+    x : int
+        x-index of coverage patch. x=0 runs from RA 0 to 10,, etc.
+    y : in
+        y-index of coverage patch. y=0 runs from dec = -90 to -80, etc.
     """
 
     __tablename__ = "depth_one_sky_coverage"
 
-    patch_id: int = Field(primary_key=True)
-
-    x: int = Field(index=True)
-    y: int = Field(index=True)
+    x: int = Field(index=True, primary_key=True)
+    y: int = Field(index=True, primary_key=True)
 
     map_id: int = Field(
-        foreign_key="depth_one_maps.map_id", nullable=False, ondelete="CASCADE"
+        foreign_key="depth_one_maps.map_id", nullable=False, ondelete="CASCADE", primary_key=True,
     )
     map: DepthOneMapTable = Relationship(back_populates="depth_one_sky_coverage")
+
+    __table_args__ = (
+        Index(
+            "ix_depth_one_sky_coverage_map_id_x_y",
+            "map_id",
+            "x",
+            "y",
+            unique=True,
+            pimary_key=True,
+        )
+    )

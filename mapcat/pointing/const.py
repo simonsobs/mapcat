f7c6123462dc69a443505dcc75fb8d0134417043
Allen M. Foster <amfoster@tiger3.princeton.edu>
2026-04-23 14:32:05 -0400

    fix astropydantic call

--- mapcat/pointing/const.py
+++ mapcat/pointing/const.py
@@ -6,16 +6,16 @@ from typing import Literal
 
 from astropy.coordinates import SkyCoord
 from astropy import units as u
-from astropydantic import AstroPydanticUnit
+from astropydantic import AstroPydanticQuantity
 from mapcat.pointing.base import PointingModelProtocol
 
 
 class ConstantPointingModel(PointingModelProtocol):
     model_type: Literal["constant"] = "constant"
 
-    ra_offset: AstroPydanticUnit[u.deg]
-    dec_offset: AstroPydanticUnit[u.deg]
-    
+    ra_offset: AstroPydanticQuantity[u.deg]
+    dec_offset: AstroPydanticQuantity[u.deg]
+
     def predict(self, pos: SkyCoord) -> SkyCoord:
         ra = pos.ra + self.ra_offset
         dec = pos.dec + self.dec_offset

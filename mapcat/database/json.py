b3a9a796393fe39d8f73dc4797dbac684b33ad9f
Allen M. Foster <amfoster@tiger3.princeton.edu>
2026-04-28 14:23:03 -0400

    ok, it needs the type adaptor

--- mapcat/database/json.py
+++ mapcat/database/json.py
@@ -4,8 +4,7 @@ A custom SQLAlchemy type to (de)serialize pydantic models to JSONB.
 See: https://github.com/fastapi/sqlmodel/pull/1324 - this can be removed at some point.
 """
 
-import typing
-
+from pydantic import TypeAdapter
 from sqlalchemy.types import JSON, TypeDecorator
 
 
@@ -16,6 +15,7 @@ class JSONEncodedPydantic(TypeDecorator):
     def __init__(self, pydantic_class, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.pydantic_class = pydantic_class
+        self._adapter = TypeAdapter(pydantic_class)
 
     def process_bind_param(self, value, dialect):
         if value is None:
@@ -25,8 +25,4 @@ class JSONEncodedPydantic(TypeDecorator):
     def process_result_value(self, value, dialect):
         if value is None:
             return None
-        cls = self.pydantic_class
-        args = typing.get_args(cls)
-        if args:
-            cls = next(a for a in args if a is not type(None))
-        return cls.model_validate(value)
+        return self._adapter.validate_python(value)

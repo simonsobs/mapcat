9deefe052c2a1ff11cb24df03d4fbafe9983cf49
Josh Borrow <josh@joshborrow.com>
2026-05-20 16:10:51 -0400

    Formatting

--- mapcat/alembic/versions/46575bc0d660_remove_requirement_for_intensity_map_.py
+++ mapcat/alembic/versions/46575bc0d660_remove_requirement_for_intensity_map_.py
@@ -5,15 +5,15 @@ Revises: a1b2c3d4e5f6
 Create Date: 2026-05-20 16:08:14.624772
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '46575bc0d660'
-down_revision: Union[str, None] = 'a1b2c3d4e5f6'
+revision: str = "46575bc0d660"
+down_revision: Union[str, None] = "a1b2c3d4e5f6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

726359bf4c16be8f4ba99bb538e8f74a44fde292
Allen Foster <amfoster@princeton.edu>
2026-08-31 14:00:26 -0400

    fix with ruff 0.16.1

--- mapcat/alembic/versions/1ea365628633_merge_heads.py
+++ mapcat/alembic/versions/1ea365628633_merge_heads.py
@@ -5,17 +5,13 @@ Revises: 0762b3c7694d, fa43e5586322
 Create Date: 2026-08-31 13:46:54.313874
 
 """
-from typing import Sequence, Union
-
-from alembic import op
-import sqlalchemy as sa
-
+from collections.abc import Sequence
 
 # revision identifiers, used by Alembic.
 revision: str = '1ea365628633'
-down_revision: Union[str, None] = ('0762b3c7694d', 'fa43e5586322')
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = ('0762b3c7694d', 'fa43e5586322')
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
